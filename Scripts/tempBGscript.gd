233c9e33784b653cbeee0a2b55878dec2f922467
Bungus <noguesaaron@gmail.com>
2023-12-23 12:49:40 -0500

    added the canvas modulate
    WARNING LEVEL 0 AND LEVEL 1 WILL BE DELETED CHANGING THE TILEMAPS CORRUPTED THEM LMAO

--- Scripts/tempBGscript.gd
+++ Scripts/tempBGscript.gd
@@ -2,4 +2,4 @@ extends ColorRect
 
 @export var player : CharacterBody2D
 func _process(delta):
-	position = player.position - Vector2(size.x/2, size.y/2)
+	position = $"../Player".position - Vector2(size.x/2, size.y/2)
